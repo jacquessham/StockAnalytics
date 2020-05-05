d15387816f3a51fdad6a8135a1ae61aa4b1c4306
Jacques Sham <jacquessham2015@gmail.com>
2020-05-04 21:45:27 -0700

    Simple Dashboard finish 2nd tab

--- SimpleDashboard/Layout.py
+++ SimpleDashboard/Layout.py
@@ -5,36 +5,120 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 
-# Layouts for Tab 1
-# Dropdown list for single choice
-def get_info_box():
+##### Layouts for Tab 1 #####
+# Dropdown list for single stock choice
+def get_tab1_info_box():
 	return html.Div([
 				html.Div([
 					html.H3(id='tab1-stock-price'),
 					html.H4(id='tab1-stock-price-change'),
 					html.H4(id='tab1-stock-price-percentchange')
-					],style={'width':'60%','display': 'inline-block'}),
+					],style={'width':'40%','display': 'inline-block'}),
+				html.Div(style={'width':'20%','display': 'inline-block'}),
 				html.Div([
-					dcc.Dropdown(id='tab1-stock-dropdown')
+					html.Div(dcc.Input(id='tab1-ticker-input',value='',
+					                   type='text'),
+					                   style={'width':'25%',
+					                          'display': 'inline-block'}),
+					html.Div(html.Button('Submit',id='tab1-submit'),
+						     style={'width':'20%','display': 'inline-block'})
 					],style={'width':'40%','display': 'inline-block'})
 			])
 
-# Layout for both Tab 1 and Tab 2
+##### Layout for Tab 2 #####
+# Dropdown list for index and stock growth difference
+def get_tab2_info_box():
+	return html.Div([
+			html.Div([dcc.Dropdown(id='tab2-index-choice',
+				                   options=index_choice,
+				                   value=index_choice[0]['value'],
+								   style={'text-align':'left'})],
+				style={'width':'30%','display': 'inline-block'}),
+			html.Div(style={'width':'20%','display': 'inline-block'}),
+			html.Div([dcc.Dropdown(id='tab2-stock-include',
+				                   options=[],
+				                   value=[],
+				                   multi=True,
+				                   style={'text-align':'left'})],
+				style={'width':'50%','display': 'inline-block'})
+		])
+
+##### Layout for both Tab 1 and Tab 2 #####
+# Layout for user to select time interval
 def get_interval_layout(tab):
 	id_name = tab+'-time-interval'
-	return html.Div([dcc.RadioItems(id=id_name)])
+	layout = html.Div(
+		dcc.RadioItems(id=id_name,
+                       options=[{'label':'1 Month', 'value':'1mo'},
+                                {'label':'3 Months', 'value':'3mo'},
+                                {'label':'6 Months', 'value':'6mo'},
+                                {'label':'YTD', 'value':'ytd'},
+                                {'label':'1 Year', 'value':'1y'},
+                                {'label':'3 Years', 'value':'3y'},
+                                {'label':'5 Years', 'value':'5y'}
+                       ],
+                       value='ytd'
+		), style={'text-align':'center'})
+	return layout
 
+# Layout for graph, include time interval and graph
+def get_graph_layout(tab):
+	layout = html.Div([
+		html.Br(),
+		get_interval_layout(tab),
+		dcc.Graph(id=tab+'-vis')
+		])
+	return layout
+
+# Layout for table for stats
 def get_table_layout(tab):
 	id_name = tab+'-table'
-	return html.Div(html.Table(id=id_name))
+	return html.Div([html.Br(),html.Br(),html.Table(id=id_name)])
 
+# Layout for stats and graph
+def get_stats_graph_layout(tab):
+	return html.Div([
+		html.Div(get_table_layout(tab),
+			     style={'width':'30%','display': 'inline-block',
+			            'vertical-align':'top'}),
+		html.Div(get_graph_layout(tab),
+			     style={'width':'70%','display': 'inline-block',
+			            'vertical-align':'top'})
+		])
 
+##### Generate Plots and Tables#####
+# Generate Line charts for stocks and index
+def getLinePlot(df):
+	traces = []
+	for col in df.columns:
+		if col != 'Date':
+			traces.append({'x':df['Date'], 'y':df[col],'name':col,
+				           'mode':'lines'})
+	layout = {'xaxis':{'title':'Date'},
+	          'yaxis':{'title':'% Change', 'tickformat':'.0%'},
+	          'hovermode':False}
+	return {'data':traces, 'layout':layout}
 
+# Generate Table for Tab 2 - Index
+def getTab2Table(name, last_close, range_period, range_52weeks):
+	if last_close >= 0:
+		color = 'green'
+	else:
+		color = 'red'
+	last_close = last_close = f'{last_close:.2f}%'
+	return html.Table([
+		html.Tr(html.Td(html.B(name))),
+		html.Tr([html.Td('Close'),
+			    html.Td(last_close, style={'color':color})]),
+		html.Tr([html.Td('Period Range'),
+			     html.Td(f'{range_period[0]:,.2f}'+ \
+			     	     ' - '+f'{range_period[1]:,.2f}')]),
+		html.Tr([html.Td('52 Weeks Range'),
+			     html.Td(f'{range_52weeks[0]:,.2f}'+\
+			     	     ' - '+f'{range_52weeks[1]:,.2f}')])
+		])
 
-def get_single_dropdown(options):
-	return dcc.Dropdown(
-		id='stock-tab1-dropdown',
-		options=[{'label':opt, 'label':opt} for opt in options],
-		value='00005',
-		style={'text-align':'left'}
-		)
+##### Global Variables #####
+# Dictionary for the dropdown list to select index in Tab 2
+index_choice = [{'label':' Hong Kong: Heng Seng Index', 'value':'hsi'},
+                {'label':' United States: S&P 500', 'value':'sp500'}]
--- SimpleDashboard/SimpleDashboard.py
+++ SimpleDashboard/SimpleDashboard.py
@@ -1,67 +1,187 @@
 import pandas as pd
-import plotly.graph_objs as go
+import re
+from datetime import datetime
+from datetime import timedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import quandl
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import yfinance
 from Layout import *
 
-"""
-def getAPI(filepath):
-	with open(filepath, 'r') as f:
-		rawtext = f.read()
-	textlist = rawtext.split(',')
-	return textlist[1]
 
-quandl.ApiConfig.api_key = getAPI('../../../../License/QuandlAPIkey.csv')
-
-start_date  = '2010-01-01'
-end_date = '2020-04-30'
-data = quandl.get("HKEX/00005", start_date=start_date,
-	              end_date=end_date).reset_index()
-"""
-
+##### Dashboard layout #####
 # Dash Set up
 app = dash.Dash()
 
-# Dashboard layout
+# Base Layout
 app.layout = html.Div([
-	dcc.Tabs(id='dashboard-tabs', value='price-tab',children=[
+	dcc.Tabs(id='dashboard-tabs', value='change-tab',children=[
 		dcc.Tab(label='Stock Price', value='price-tab',children=[
-			html.Div([html.H2('Cathay Pacific', 
+			html.Div([html.H2(id='tab1-stock-name', 
 				           style={'width':'30%','display':'inline-block'}), 
-				      html.H4('00293.HK', 
+				      html.H4(id='tab1-ticker', 
 				           style={'width':'10%','display':'inline-block'})],
-				      style={'width':'70%','margin':'auto'}),
+				      style={'width':'90%','margin':'auto'}),
 			# Position 0, Title
-			html.Div(get_info_box(),
-				     style={'width':'70%','margin':'auto'}),
+			html.Div(get_tab1_info_box(),
+				     style={'width':'90%','margin':'auto'}),
 			# Position 1, Info and dropdown
-			html.Div(get_interval_layout('tab1'),
-				     style={'width':'70%','margin':'auto'}),
-			# Position 2, Range
-			html.Div(dcc.Graph(id='price-vis'),
-				     style={'width':'80%','margin':'auto'}),
-			# Position 3, Graph
-			html.Div(get_table_layout('tab1'),
-				     style={'width':'80%','margin':'auto'})
-			# Position 4, Table of stats
+			html.Div(get_stats_graph_layout('tab1'),
+				     style={'width':'90%','margin':'auto'})
+			# Position 2, Table of stats and graph
 			]), # Tab 1, End price-tab
-		dcc.Tab(label='Index Change', value='change-tab',children=[
-			html.Div([html.H2('Price Change')]),
+		dcc.Tab(label='Stock/Index Growth', value='change-tab',children=[
+			html.Div([html.H2('Stock Price Growth vs. Index Growth')],
+				     style={'width':'90%','margin':'auto',
+				            'text-align':'center'}),
 			# Position 0, Title
-			html.Div([html.P('Position 1')]),
+			html.Div(get_tab2_info_box(),
+				     style={'width':'90%','margin':'auto'}),
 			# Position 1, Info and dropdown
-			html.Div(get_interval_layout('tab2')),
-			# Position 2, Range
-			html.Div(dcc.Graph(id='change-vis')),
-			# Position 3, Graph
-			html.Div(get_table_layout('tab2'))
-			# Position 4, Table of stats
+			html.Div(get_stats_graph_layout('tab2'),
+				     style={'width':'90%','margin':'auto'})
+			# Position 2, Table of stats and graph
 			]) # Tab 2, End change-tab
 		]) # End Tabs
 	]) # End base Div
+
+##### Callbacks for Tab 1 ######
+@app.callback([Output('tab1-stock-name','children'),
+	           Output('tab1-ticker','children'),
+	           Output('tab1-stock-price','children')],
+	          [Input('tab1-submit','n_clicks')],
+	          [State('tab1-ticker-input','value')])
+def get_ticker(n_clicks, ticker):
+	# Identify which ticker is that
+	tick = re.findall('\d{1,5}|[A-Za-z]{1,4}', ticker)
+	if len(tick)>0:
+		print(tick[0])
+	return 'Cathay Pacific', '00293.HK', '$20.00'
+
+##### Callbacks for Tab 2 ######
+"""
+To generate the options to display on second dropdown for users to include
+stock(s) addition to index.
+
+Tab 2 only relies on data from Yahoo Finance
+"""
+"""
+This callback return the list of stocks of corresponsed market. It requires
+user to input which market to look and return 2 outputs.
+1- List of stocks in the multi-dropdown list
+2- The default value of the multi-dropdown list, in order to unselect previous
+   stocks.
+"""
+@app.callback([Output('tab2-stock-include','options'),
+	           Output('tab2-stock-include','value')],
+	           [Input('tab2-index-choice','value')])
+def generate_dropdown_stocknames(mkt):
+	if mkt == 'hsi':
+		stock_list = pd.read_csv('Data/HengSengStockList.csv', dtype=str)
+		stock_list = stock_list.sort_values('Ticker')
+		stock_list['label'] = stock_list['Ticker'].astype(str) + '\t' + \
+		                      stock_list['Company']
+		opts = [{'label': label, 'value': ticker} for label, ticker in 
+		        zip(stock_list['label'].tolist(),
+		        	stock_list['Ticker'].tolist())]
+		return opts, []
+	elif mkt == 'sp500':
+		stock_list = pd.read_csv('Data/SP500StockList.csv', engine='python')
+		stock_list = stock_list.sort_values('Ticker')
+		stock_list['label'] = stock_list['Ticker']
+		opts = [{'label': label, 'value': ticker} for label, ticker in 
+		        zip(stock_list['label'].tolist(),
+		        	stock_list['Ticker'].tolist())]
+		return opts, []
+	return [], []
+
+"""
+This callback generates Graph and the summary table of statistic.
+The steps of this completing this functions:
+1- Obtain the inputs, first get a list of stocks along with which index
+2- If there is at least one stock selected other than the index, obtain
+   the data frame of stock price. Since there is no stock selected for
+   default setting, this step will be skipped if there is no stock
+   selected
+3- Take the price of the first day of the time interval, calculate the
+   price change in percentage for each day relatively to the first day.
+4- Generate a line chart from the data prepared in step 3
+5- Generate a html table for the summary of index statistics
+"""
+@app.callback([Output('tab2-table','children'),
+	           Output('tab2-vis','figure')],
+	          [Input('tab2-index-choice','value'),
+	           Input('tab2-stock-include','value'),
+	           Input('tab2-time-interval','value')])
+def generate_tab2_graph(mkt,stocks,time):
+	# Function to calculate price change relative to the first day
+	def get_price_change(price_list):
+		base_price = price_list[0]
+		return [(price/base_price)-1 for price in price_list]
+	# Obtain the histoical stock price of selected stocks
+	df_stocks = []
+	if len(stocks) > 0:
+		for stock in stocks:
+			if mkt=='hsi':
+				stock = stock[1:] + '.HK'
+			stock_df = yfinance.Ticker(stock).history(period=time)
+			stock_df = stock_df.reset_index()[['Date','Close']]
+			stock_df.columns = ['Date',stock]
+			df_stocks.append(stock_df)
+
+	"""
+	Obtain the historical index and with stock price dataframe.
+	Then, convert to percent change relative to first day in the data frame.
+	"""
+	y_ticker = None
+	index_col = None
+	if mkt == 'hsi':
+		y_ticker = '^HSI'
+		index_col = 'Heng Seng Index'
+
+	elif mkt == 'sp500':
+		# Note that Yahoo use ^GSPC as SP 500 ticker, SPX only has 1 entry
+		y_ticker = '^GSPC'
+		index_col = 'S&P 500'
+
+	# Prevent error if nothing selected
+	else:
+		return html.Table(), {'data': None}
+
+	# Prepare the data set to plot the line chart
+	index = yfinance.Ticker(y_ticker)
+	df_index = index.history(period=time).reset_index()[['Date','Close']]
+	df_index.columns = ['Date', index_col]
+	# To take out duplicated columns, ie, Date, while concat
+	if len(stocks) > 0:
+		for df_temp in df_stocks:
+			df_index = pd.merge(df_index, df_temp, how='left',on='Date')
+		# In case there are NA's, fill with last observation
+		df_index = df_index.fillna(method='backfill', axis=1)
+		# Drop duplicated columns in case there are
+		df_index = df_index.loc[:,~df_index.columns.duplicated()]
+
+	# Convert from price to percent change relative to Day 1 of the period
+	for col in df_index.columns:
+		if col != 'Date':
+			df_index[col] = get_price_change(df_index[col].tolist())
+	fig = getLinePlot(df_index)
+
+	# Prepare the data set to list the summary on the table
+	last_close = df_index[index_col].tolist()[-1]*100
+	df_index_period = index.history(period=time).reset_index()
+	df_index_52weeks = index.history(period='1y').reset_index()
+	range_period = (round(df_index_period['Close'].min(),2), 
+					round(df_index_period['Close'].max(),2))
+	range_52weeks = (round(df_index_52weeks['Close'].min(),2), 
+					 round(df_index_52weeks['Close'].max(),2))
+	volume = (index.history(period=time).reset_index()['Volume'].tolist()[-1],
+	          index.history(period=time).reset_index()['Volume'].mean())
+	# Generate a table of summary
+	table = getTab2Table(index_col, last_close, range_period, range_52weeks)
+
+	return table, fig
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8000)
